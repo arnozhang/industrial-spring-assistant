8fa613f69ed95c765e56d39b815d26585563f9ec
牧秦丶 <zyfgood12@gmail.com>
2021-02-19 14:46:25 +0800

    Add CSS-Modules support in webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         test: /\.less$/,
         use: [
           'style-loader',
-          'css-loader',
+          'css-loader?modules',
           'less-loader',
         ],
       },

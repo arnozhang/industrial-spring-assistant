ff24c3f10d530ef5aa3bc047836b43cb9e1f350f
牧秦丶 <zyfgood12@gmail.com>
2021-02-19 20:48:09 +0800

    Add picture for butterfly helper.

--- src/render/pages/ButterflySpring/helper.tsx
+++ src/render/pages/ButterflySpring/helper.tsx
@@ -10,11 +10,30 @@ import * as React from 'react';
 
 const ButterflySpringHelper = () => {
   return (
-    <div>
+    <div className="flex-column">
       <p>① 应力计算结果，正直为拉应力，负值为压应力。</p>
       <p>② 静载荷：σOM &lt;= σs；无支撑面的变载荷：结合上图找疲劳破坏点。</p>
       <p>③ 变载荷的安装，必须有预压变形：0.15h0 &lt;= f &lt;= 0.2h0</p>
       <p>④ 疲劳校核：略（见手册）</p>
+
+      <img
+        style={{
+          width: '100%',
+          padding: '10px 40px',
+        }}
+        src="../images/butterfly-spring/position.png"
+        alt=""
+      />
+
+      <img
+        style={{
+          width: '60%',
+          padding: '10px 40px',
+          alignSelf: 'center',
+        }}
+        src="../images/butterfly-spring/cd.png"
+        alt=""
+      />
     </div>
   );
 };
